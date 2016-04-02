a170b1e00173de8c2ccf5ea2ee1e5191d2f78485
ml4963 <ml4963@nyu.edu>
2016-04-02 17:57:40 -0400

    adding~

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 # flaskr.py    
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 from app import views
+from clarifai.client import ClarifaiApi
 
 app = Flask(__name__)
 
 @app.route('/')
-def home(): #homepage; gotta edit the 
+def home(): #homepage; gotta edit the
+    if request.method == 'GET':
+        url = request.form['url']
+        return redirect(url_for('tags')
+                        
+@app.route('/tags')
+def tags():
+    # here is where the clarifai api will take in the url
     return render_template('welcome.html') 
 
 if __name__ == "__main__":

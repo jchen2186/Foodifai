9315e816ff6cc5b1d43633577d5a64b9cdcac749
ml4963 <ml4963@nyu.edu>
2016-04-02 17:23:01 -0400

    adding stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 # flaskr.py    
 from flask import Flask
+from app import views
 
 app = Flask(__name__)
 
-from app import views
+@app.route('/')
+def home(): #homepage; gotta edit the 
+    return render_template('welcome.html') 
 
 if __name__ == "__main__":
     app.run()

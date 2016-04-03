fc5020e70c23501615ceb4ff43634a48ac2196d6
ml4963 <ml4963@nyu.edu>
2016-04-02 20:35:21 -0400

    adding~

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,26 @@
 # flaskr.py    
 from flask import Flask, request, render_template, redirect, url_for
-from app import views
+# from app import views
 from clarifai.client import ClarifaiApi
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
-def home(): 
-    if request.method == 'GET':
-        url = request.form['url']
-        clarifai_api = ClarifaiApi(model='EDITHERE')
-        result = clarifai_api.tag_image_url(#SOMETHING)
-        return(result)    
-        return redirect(url_for('tags'))
-                        
-@app.route('/tags')
+def home():
+    return render_template('welcome.html')
+
+@app.route('/tags', methods=['GET'])
 def tags():
-    # here is where the clarifai api will take in the url
-    return render_template('welcome.html') 
+    return(request.args["url"])
+                        
 
 if __name__ == "__main__":
     app.run()
+
+# https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/2007feb-sushi-odaiba-manytypes.jpg/435px-2007feb-sushi-odaiba-manytypes.jpg
+
+'''
+
+        clarifai_api = ClarifaiApi(model='EDITHERE')
+        result = clarifai_api.tag_image_url(#SOMETHING)
+        '''

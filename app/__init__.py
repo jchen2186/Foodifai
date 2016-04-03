a7546cff5f43a19342d14bb248f9b62958a0ff84
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-04-02 21:31:10 -0400

    got image from url

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,34 @@
-# flaskr.py    
+# flaskr.py
 from flask import Flask, request, render_template, redirect, url_for
 # from app import views
 from clarifai.client import ClarifaiApi
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('welcome.html')
 
+
 @app.route('/tags', methods=['GET'])
 def tags():
-    return(request.args["url"])
-                        
+    url = request.args["url"]
+    # pathName = "templates/tags.html"
+    fo = open("tags.html", 'w')
+    html_str = '''
+    <html>
+        <head></head>
+        <body><img src ="%s" /></body>
+    </html>
+    '''
+    fo.write(html_str % url)
+
+    return html_str % url
 
 if __name__ == "__main__":
     app.run()

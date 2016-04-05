c440524f2f123c3f89e4d7f3d40f785ffed6dd2b
ml4963 <ml4963@nyu.edu>
2016-04-05 15:05:28 -0400

    95% finished?

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-# flaskr.py
+
+#problem: figure out how to obtain address of user without asking
+
 from flask import Flask, request, render_template, redirect, url_for
 # from app import views
 from clarifai.client import ClarifaiApi
@@ -8,9 +10,16 @@ from os import listdir
 from os.path import isfile, join
 from clarify_python import clarify
 import foursquare
+from geopy.geocoders import Nominatim
 
-app = Flask(__name__) #, template_folder=template_path)
+app = Flask(__name__) 
 
+# converting the obtained, raw data of an address into a string
+def newAddress(address):
+    anonAddress = ""
+    for i in address:
+        anonAddress = anonAddress + " " + i
+    return(anonAddress)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -26,12 +35,19 @@ def tags():
     client = foursquare.Foursquare(client_id='JEK02X44TGMNJSE0VC1UBEB4FRNNW3UMFQ4IQOPI4BAR2GXA', \
                                     client_secret='A2Z50VTUHHXEUYJBHCQKB1LXTNVVBYBQR4SDASVTXTWUMWXS') #foursquare shit
 
+    address = request.args["address"] #address is currently a string
+    geolocator = Nominatim()
+    location = geolocator.geocode(address)
+    newLocation = str(location.latitude) + str(", ") + str(location.longitude)
     
-    foursquare_dictionary = client.venues.explore(params={'ll': '40.7, -74', 'v': '20160402', 'query': results[0] + ',' + results[1] + ',' + results[2]})
+    
+    # foursquare_dictionary = client.venues.explore(params={'ll': '40.7, -74', 'v': '20160402', 'query': results[0] + ',' + results[1] + ',' + results[2]})
+
+    foursquare_dictionary = client.venues.explore(params={'ll': newLocation, 'v': '20160402', 'query': results[0] + ',' + results[1] + ',' + results[2]})
 
     #first place to eat
     # status1 = foursquare_dictionary['groups'][0]['items'][0]['venue']['hours']['status']
-    address = foursquare_dictionary['groups'][0]['items'][0]['venue']['location']['formattedAddress']
+    address1 = foursquare_dictionary['groups'][0]['items'][0]['venue']['location']['formattedAddress']
     name = foursquare_dictionary['groups'][0]['items'][0]['venue']['name']
 
     #second place to eat
@@ -39,22 +55,11 @@ def tags():
    # status2 = foursquare_dictionary['groups'][0]['items'][1]['venue']['hours']['status']
     name1 = foursquare_dictionary['groups'][0]['items'][1]['venue']['name']
 
-
-    newAddress = ""
-    for i in address:
-        newAddress = newAddress + " " + i
-
-    newAddress1 = ""
-    for i in address2:
-        newAddress1 = newAddress1 + " " + i
-        
-    
     return render_template('tags.html',\
-                           newAddress = newAddress, name = name,\
-                           newAddress1 = newAddress1, name1 = name1) #status2 = status2)#\
-                           #newAddress2 = newAddress2, name2 = name2, status3 = status3)
+                           newAddress1 = newAddress(address1), name = name,\
+                           newAddress2 = newAddress(address2), name1 = name1,\
+                           newLocation = newLocation)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
- 
